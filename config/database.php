eb3f69742cc5d135b6a5f153ef2310b947d27767
Rodrigo Alves <rodrigo.souza@sumup.com>
2019-07-23 00:15:52 -0300

    set default credentials for database

--- config/database.php
+++ config/database.php
@@ -6,9 +6,9 @@
                 'driver' => 'mongodb',
                 'host' => env('DB_HOST', 'localhost'),
                 'port' => env('DB_PORT', 27017),
-                'database' => env('DB_DATABASE'),
-                'username' => env('DB_USERNAME'),
-                'password' => env('DB_PASSWORD'),
+                'database' => env('DB_DATABASE', 'diary-api'),
+                'username' => env('DB_USERNAME', ''),
+                'password' => env('DB_PASSWORD', ''),
                 'options' => [
                     'database' => 'admin' // sets the authentication database required by mongo 3
                 ]
