d211a72392961ee6f98f8cc9cc8232410934f543
R. A. Souza <alves.wm@gmail.com>
2020-06-27 15:23:59 -0300

    code coverage ignore files

---
+++ app/Providers/OAuthServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class OAuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('League\OAuth2\Client\Provider\Github', function () {
+            return new Github([
+                'clientId' => env('GITHUB_CLIENT_ID'),
+                'clientSecret' => env('GITHUB_CLIENT_SECRET'),
+                'redirectUri' => env('APP_URL') . '/oauth2/github/callback'
+            ]);
+        });
+
+        $this->app->bind('League\OAuth2\Client\Provider\GenericProvider', function () {
+            return new GenericProvider([
+                'clientId'                => env('IDP_CLIENT_ID'),
+                'clientSecret'            => env('IDP_CLIENT_SECRET'),
+                'redirectUri'             => env('APP_URL') . '/oauth2/callback',
+                'urlAuthorize'            => env('IDP_EXTERNAL_URL') . '/oauth2/auth',
+                'urlAccessToken'          => env('IDP_URL') . '/oauth2/token',
+                'urlResourceOwnerDetails' => env('IDP_URL') . '/userinfo'
+            ]);
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
